acf49becadb215673c99fef5dd6889ef9da3ec56
DmitryChitalov <cdi9@yandex.ru>
2024-01-21 18:33:18 +0500

    !

---
+++ hh_parser/web_app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+SECRET_KEY = b'\x143#\x1eV;\xc9\xa0\xecr\r\xd4/{b\n'
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+def create_app():
+    from .flask_parser.flask_parser import parser_blueprint
+    app.register_blueprint(parser_blueprint)
+    from .authorization.auth import auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+
+
+    return app
---
+++ hh_parser/web_app/hh_flask_parser.py
@@ -0,0 +1,14 @@
+import multiprocessing as ml
+from hh_parser.parser_app import main as pr
+
+from hh_parser.web_app import create_app
+
+if __name__ == "__main__":
+    # Создать приложение Flask
+    app = create_app()
+
+    par_service = ml.Process(name="HH API Parser", target=pr.main)
+    par_service.start()
+
+    # Запустить Flask приложение
+    app.run()
